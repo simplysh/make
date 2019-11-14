128a9eaf84c81f5014bbff61c9a6a8e849c46600
Andrei Nemes <simply.andrei@gmail.com>
2019-11-14 16:48:23 +0000

    fix example program

--- main.cpp
+++ main.cpp
@@ -27,12 +27,31 @@ int main(int argc, char* argv[]) {
   screen = SDL_GetWindowSurface(window);
   renderTarget = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+  // initialise stuff
+  if (window == NULL) {
+    std::cout << "Can't draw window: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+  if (TTF_Init() < 0) {
+    std::cout << "Can't initialize TTF: " << TTF_GetError() << std::endl;
+    return 1;
+  }
+  const int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+  if (!IMG_Init(imgFlags) & imgFlags) {
+    std::cout << "Can't initialize IMG: " << IMG_GetError() << std::endl;
+    return 1;
+  }
+
   // create the font
   const std::string textSource = "BalooBhaina-Regular.ttf";
   const std::string textMessage = "You're all set! :)";
-  SDL_Color white = { 255, 255, 255, 255 };
+  SDL_Color white = { 255, 255, 255 };
 
-  TTF_Font* font = TTF_OpenFont(textSource.c_str(), 100);
+  TTF_Font* font = TTF_OpenFont(textSource.c_str(), width / 10);
+  if (!font) {
+    printf("TTF_OpenFont: %s\n", TTF_GetError());
+  }
+
   SDL_Surface* textSurface = TTF_RenderText_Blended(font, textMessage.c_str(), white);
   SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderTarget, textSurface);
 
@@ -49,7 +68,7 @@ int main(int argc, char* argv[]) {
   bool running = true;
   SDL_Event event;
 
-  SDL_SetRenderDrawColor(renderTarget, 150, 50, 100, 255);
+  SDL_SetRenderDrawColor(renderTarget, 132, 63, 91, 255);
 
   while (running) {
     while (SDL_PollEvent(&event) != 0) {
